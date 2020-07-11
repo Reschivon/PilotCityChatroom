113b912a52c229f5b5bf38761fcf31117402ac1f
YuraT <ytatisch@gmail.com>
2020-07-11 10:48:48 -0700

    configure auth and experimental github support

--- backend/src/services/users/users.class.js
+++ backend/src/services/users/users.class.js
@@ -1,4 +1,19 @@
+// This is the database adapter service class
 const { Service } = require('feathers-mongodb');
+// We need this to create the MD5 hash
+const crypto = require('crypto');
+
+// The Gravatar image service
+const gravatarUrl = 'https://s.gravatar.com/avatar';
+// The size query. Our chat needs 60px images
+const query = 's=60';
+// Returns the Gravatar image for an email
+const getGravatar = email => {
+  // Gravatar uses MD5 hashes from an email address (all lowercase) to get the image
+  const hash = crypto.createHash('md5').update(email.toLowerCase()).digest('hex');
+  // Return the full avatar URL
+  return `${gravatarUrl}/${hash}?${query}`;
+};
 
 exports.Users = class Users extends Service {
   constructor(options, app) {
@@ -8,4 +23,22 @@ exports.Users = class Users extends Service {
       this.Model = db.collection('users');
     });
   }
+
+  create (data, params) {
+    // This is the information we want from the user signup data
+    const { email, password, githubId, name } = data;
+    // Use the existing avatar image or return the Gravatar for the email
+    const avatar = data.avatar || getGravatar(email);
+    // The complete user
+    const userData = {
+      email,
+      name,
+      password,
+      githubId,
+      avatar
+    };
+
+    // Call the original `create` method with existing `params` and new data
+    return super.create(userData, params);
+  }  
 };

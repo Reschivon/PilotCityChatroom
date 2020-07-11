dfc65365d51af4ec2a360c4ea1871f71ac8a6d1f
YuraT <ytatisch@gmail.com>
2020-07-11 10:49:19 -0700

    add messages service and hooks

---
+++ backend/src/hooks/process-message.js
@@ -0,0 +1,31 @@
+// Use this hook to manipulate incoming or outgoing data.
+// For more information on hooks see: http://docs.feathersjs.com/api/hooks.html
+
+// eslint-disable-next-line no-unused-vars
+module.exports = (options = {}) => {
+  return async context => {
+    const { data } = context;
+
+    // Throw an error if we didn't get a text
+    if(!data.text) {
+      throw new Error('A message must have a text');
+    }
+
+    // The logged in user
+    const { user } = context.params;
+    // The actual message text
+    // Make sure that messages are no longer than 400 characters
+    const text = data.text.substring(0, 400);
+
+    // Update the original data (so that people can't submit additional stuff)
+    context.data = {
+      text,
+      // Set the user id
+      userId: user._id,
+      // Add the current date
+      createdAt: new Date().getTime()
+    };
+    
+    return context;
+  };
+};
---
+++ backend/src/services/messages/messages.hooks.js
@@ -0,0 +1,36 @@
+const { authenticate } = require('@feathersjs/authentication').hooks;
+
+const populateUser = require('../../hooks/populate-user');
+const processMessage = require('../../hooks/process-message');
+
+module.exports = {
+  before: {
+    all: [ authenticate('jwt') ],
+    find: [],
+    get: [],
+    create: [processMessage()],
+    update: [],
+    patch: [],
+    remove: []
+  },
+
+  after: {
+    all: [populateUser()],
+    find: [],
+    get: [],
+    create: [],
+    update: [],
+    patch: [],
+    remove: []
+  },
+
+  error: {
+    all: [],
+    find: [],
+    get: [],
+    create: [],
+    update: [],
+    patch: [],
+    remove: []
+  }
+};

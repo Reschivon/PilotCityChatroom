a4f12333d4edf1407904dd02f821a761bcec5d81
Radagast-the-infinite <ng.kyle101@gmail.com>
2020-07-12 22:37:24 -0700

    Made the Boilerplate for settings page

---
+++ frontend/src/views/settings/Settings.vue
@@ -0,0 +1,26 @@
+<template>
+    <div class="">
+        <h>Settings Page</h>
+    </div>
+</template>
+
+<script>
+
+export default {
+  name: "Settings",
+  components: {
+
+  },
+  data () {
+    return {
+     
+    }
+  },
+}
+</script>
+
+<style>
+
+
+
+</style>
